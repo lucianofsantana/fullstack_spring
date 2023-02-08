8c05829ef57ad1dd5463d80707b0d64ec7778892
Luciano Santana <luciano6750@gmail.com>
2023-02-08 12:11:42 -0300

    Projeto criado

---
+++ src/test/java/com/lucianofsantana/fullstack_spring/FullstackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianofsantana.fullstack_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
