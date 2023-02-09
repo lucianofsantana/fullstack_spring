4dd97a3af249d5da5d70fb10680824c042ebec60
Luciano Santana <luciano6750@gmail.com>
2023-02-09 08:40:02 -0300

    Criando operação de Instanciação

--- src/main/java/com/lucianofsantana/fullstack_spring/FullstackSpringApplication.java
+++ src/main/java/com/lucianofsantana/fullstack_spring/FullstackSpringApplication.java
@@ -1,13 +1,32 @@
 package com.lucianofsantana.fullstack_spring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FullstackSpringApplication {
+import com.lucianofsantana.fullstack_spring.domain.Categoria;
+import com.lucianofsantana.fullstack_spring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class FullstackSpringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
