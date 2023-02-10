49ea8f7069a50f1739b62f6116fddcef088e06f7
Luciano Santana <luciano6750@gmail.com>
2023-02-10 12:46:48 -0300

    Adicionado função delete categoria

--- src/main/java/com/lucianofsantana/fullstack_spring/services/CategoriaService.java
+++ src/main/java/com/lucianofsantana/fullstack_spring/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.lucianofsantana.fullstack_spring.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.lucianofsantana.fullstack_spring.domain.Categoria;
 import com.lucianofsantana.fullstack_spring.repositories.CategoriaRepository;
+import com.lucianofsantana.fullstack_spring.services.exceptions.DataIntegrityException;
 import com.lucianofsantana.fullstack_spring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,7 +17,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		Categoria cat = obj.orElse(null);
 
@@ -30,4 +32,17 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possibel excluir uma categoria que possui produtos");
+		}
+	}
 }
